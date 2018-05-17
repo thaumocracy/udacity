501f8d767aba36aeb72ff0d7aa43356937e460f5
Winterstorm <thunpwnz@gmail.com>
2018-05-18 00:04:09 +0400

    uploaded feedreader and restaurant review

---
+++ Udacity/restaurant/js/serviceWorker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('wittr-static-v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request().then(function(response){
+            if(response){
+                return response;
+            } else {
+                fetch(event.request);
+            }
+        }))
+    )
+});
